154709dac058082c18bd704bf070e551b3c3efb5
LABBASSIBassem <49342568+LABBASSIBassem@users.noreply.github.com>
2020-05-17 23:04:34 +0100

    Update App.js

--- checkpoint/src/App.js
+++ checkpoint/src/App.js
@@ -9,9 +9,9 @@ function App() {
 
 <div style={{border:'solid 1px black', maxWidth:'100vw'}}>  
     	<h1 className="title red">Your name here</h1>  
-      <div> <br></br> </div>
+      <br/>
              <img src={Light} />
-       <div> <br></br> </div>         	
+    <br/>
              <img src="/Car.jpg"/> 
     </div>   
     <video width={320} height={240} controls>   
